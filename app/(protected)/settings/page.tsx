a210d91dc005136cab1b737e194d319707316790
uuam <chen552025@gmail.com>
2024-03-16 17:10:59 +0800

    0316)update-2

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -17,10 +17,23 @@ import {
   FormField,
   FormItem,
   FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { UserRole } from "@prisma/client";
 
 const SettingPage = () => {
+  const user = useCurrentUser();
   const [error, setError] = useState<string | undefined>();
   const [success, setSuccess] = useState<string | undefined>();
   const { update } = useSession();
@@ -28,12 +41,20 @@ const SettingPage = () => {
   const form = useForm<z.infer<typeof SettingsSchema>>({
     resolver: zodResolver(SettingsSchema),
     defaultValues: {
-      name: "",
-    },
+      // 在input框真實顯示可以刪除的那種
+      name: user?.name || undefined,
+      email: user?.email || undefined,
+      password: undefined,
+      newPassword: undefined,
+      role: user?.role || undefined,
+    }
   });
 
   const onSubmit = (values: z.infer<typeof SettingsSchema>) => {
     startTransition(() => {
+      setError("");
+      setSuccess("");
+
       settings(values)
         .then((data) => {
           if (data.error) {
@@ -57,19 +78,116 @@ const SettingPage = () => {
       </CardHeader>
       <CardContent>
         <Form {...form}>
-          <form className="space-y-6" onClick={form.handleSubmit(onSubmit)}>
-            <FormField
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Name</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-              control={form.control}
-            />
+          <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="space-y-4">
+              {/* name */}
+              <FormField
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="text"
+                        placeholder={user?.name || "change your name"}
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                    <FormMessage/>
+                  </FormItem>
+                )}
+                control={form.control}
+              />
+              {/* email */}
+              <FormField
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder={user?.email || "test@example.com"}
+                        disabled={isPending}
+                        type="text"
+                      />
+                    </FormControl>
+                    <FormMessage/>
+                  </FormItem>
+                )}
+                control={form.control}
+              />
+              {/* password */}
+              <FormField
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="password"
+                        placeholder="******"
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                    <FormMessage/>
+                  </FormItem>
+                )}
+                control={form.control}
+              />
+              {/* new password */}
+              <FormField
+                name="newPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>New Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="password"
+                        placeholder="******"
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                    <FormMessage/>
+                  </FormItem>
+                )}
+                control={form.control}
+              />
+              {/* role */}
+              <FormField
+                name="role"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Role</FormLabel>
+                    <Select
+                      disabled={isPending}
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a role" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value={UserRole.ADMIN}>Admin</SelectItem>
+                        <SelectItem value={UserRole.USER}>User</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage/>
+                  </FormItem>
+                )}
+                control={form.control}
+              />
+            </div>
+            <FormError message={error} />
+            <FormSuccess message={success} />
+            <Button disabled={isPending} type="submit" className="min-w-16">
+              {isPending ? <BeatLoader size={10} /> : "Save"}
+            </Button>
           </form>
         </Form>
       </CardContent>
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,3 +1,4 @@
+import { UserRole } from "@prisma/client";
 import * as z from "zod";
 
 export const NewPasswordSchema = z.object({
@@ -21,6 +22,27 @@ export const RegisterSchema = z.object({
   name: z.string().min(2, { message: "Name is required" }),
 });
 
-export const SettingsSchema = z.object({
-  name: z.optional(z.string()),
-});
+export const SettingsSchema = z
+  .object({
+    name: z.optional(z.string()),
+    isTwoFactorEnabeled: z.optional(z.boolean()),
+    role: z.enum([UserRole.ADMIN, UserRole.USER]),
+    email: z.optional(z.string().email()),
+    password: z.optional(z.string().min(6)),
+    newPassword: z.optional(z.string().min(6)),
+  })
+  // refine 方法用於對已定義的 schema 進行進一步的細化或驗證
+  .refine(
+    (data) => {
+      if (data.password && !data.newPassword) return false;
+      return true;
+    },
+    { message: "New password is required!", path: ["newPassword"] }
+  )
+  .refine(
+    (data) => {
+      if (data.newPassword && !data.password) return false;
+      return true;
+    },
+    { message: "Password is required!", path: ["password"] }
+  );
