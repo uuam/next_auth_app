1d6e8227d07458d5a6e40479b37137a528278b49
uuam <chen552025@gmail.com>
2024-03-24 18:32:06 +0800

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,9 @@ export const metadata: Metadata = {
     absolute: "Next Auth | 2FA",
   },
   verification: {
-    google:'5Xy4AgCm0ktuVq6l8VME36ZvjQ9rT2eSOUISypaXV3A',
+    // google:'5Xy4AgCm0ktuVq6l8VME36ZvjQ9rT2eSOUISypaXV3A',
+    google:'aVUBlVSKtubvf21M-vKyrwh_n1GvbTUFnU9KtTWUqc8'
+
   },
   description:
     "使用 Auth.js 進行用戶驗證，Next.js 框架搭建的簡易響應式網頁｜簡易的雙重驗證網頁，具有註冊登入等功能，也可使用社交媒體帳號登入｜Google | Github",
@@ -28,6 +30,7 @@ export const metadata: Metadata = {
     images: "https://next-auth-2fa.vercel.app/org_img.png",
     siteName: "Next Auth 2FA | Auth 雙重驗證",
     locale: "zh_TW",
+    url:"https://next-auth-2fa.vercel.app/"
   },
   generator: "Next.js",
   applicationName: "簡易雙重驗證",
